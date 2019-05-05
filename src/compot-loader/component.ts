a4351d934ed8499bfcac6925c9d857f9aa93c6a7
Mateusz Golewski <shirk3y@gmail.com>
2019-05-05 06:16:28 +0200

    style fix

--- src/compot-loader/component.ts
+++ src/compot-loader/component.ts
@@ -2,11 +2,12 @@ import React from "react";
 
 import { ChildInfo } from "./parser";
 
-export const createComponent = (info: ChildInfo) => props =>
-  typeof info === "string"
-    ? info
-    : React.createElement(
-        info.type,
-        { ...info.props, ...props },
-        ...info.children.map(childInfo => createComponent(childInfo)({}))
-      );
+export const createComponent = (info: ChildInfo) => props => {
+  if (typeof info === "string") return info;
+
+  return React.createElement(
+    info.type,
+    { ...info.props, ...props },
+    ...info.children.map(childInfo => createComponent(childInfo)({}))
+  );
+};
